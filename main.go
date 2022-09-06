a7f06490d18419efa75c9c85aa04954ea25810e5
liuyang <1486015534@qq.com>
2022-09-06 14:08:38 +0800

    最新

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"ttserver/config"
 	"ttserver/model"
 )
 
 func main() {
 	config.Init()
-	mysql.InitDb()
+	model.InitDb()
 	//model.CreateUserTable()
+	var user model.User
+	model.Find(&user, map[string]interface{}{"username": "liuyang"},[]string{"pass_word","UUID"})
+
+	fmt.Println(user)
 }
